41f8501237c7ceb9d7f7084436d43ddff038dd24
valmir <valmir@gmx.de>
2019-07-24 11:02:45 +0200

    added duplicate functionality, client changes

--- server/core/app/models.py
+++ server/core/app/models.py
@@ -6,8 +6,11 @@ class Source(models.Model):
   publisher = models.CharField(max_length=100)
 
   class Meta:
-    verbose_name = "Source"
-    verbose_name_plural = "Sources"
+    verbose_name = 'Source'
+    verbose_name_plural = 'Sources'
+    constraints = [
+      models.UniqueConstraint(fields=['author', 'url', 'publisher'], name='uq_source')
+    ]
 
 class Translation(models.Model):
   text = models.TextField()
@@ -15,5 +18,8 @@ class Translation(models.Model):
   source = models.ForeignKey('Source', on_delete=models.CASCADE)
 
   class Meta:
-    verbose_name = "Translation"
-    verbose_name_plural = "Translations"
+    verbose_name = 'Translation'
+    verbose_name_plural = 'Translations'
+    constraints = [
+      models.UniqueConstraint(fields=['text', 'translation'], name='uq_translation')
+    ]
--- server/core/app/views.py
+++ server/core/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.utils import IntegrityError
 
 from rest_framework import generics
 from rest_framework.response import Response
@@ -8,24 +9,32 @@ from .models import Translation, Source
 from .serializers import TranslationsSerializer
 
 class ListCreateTranslationView(generics.ListCreateAPIView):
-  """
-  GET songs/
-  POST songs/
-  """
+
   queryset = Translation.objects.all()
   serializer_class = TranslationsSerializer
 
   def post(self, request, *args, **kwargs):
-    createdSource = Source.objects.create(
-      author=request.data['source']['author'],
-      url=request.data['source']['url']
-    )
+    createdSource = self.createSource(request)
     createdTranslation = Translation.objects.create(
-      text=request.data['text'],
-      translation=request.data['translation'],
-      source=createdSource
+      text = request.data['text'],
+      translation = request.data['translation'],
+      source = createdSource
     )
     return Response(
       data=TranslationsSerializer(createdTranslation).data,
       status=status.HTTP_201_CREATED
     )
+
+  def createSource(self, request):
+    try:
+      return Source.objects.create(
+        author=request.data['source']['author'],
+        url=request.data['source']['url'],
+        publisher=request.data['source']['publisher']
+      )
+    except IntegrityError:
+      return Source.objects.get(
+        author = request.data['source']['author'],
+        url = request.data['source']['url'], 
+        publisher = request.data['source']['publisher'])
+
